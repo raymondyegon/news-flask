7b244e390a06df14485e8f3efd4d06421e6ab3e1
Raymond Yegon <raymondyegon17@gmail.com>
2019-04-12 16:18:08 +0300

    Search functionality added

--- app/request.py
+++ app/request.py
@@ -36,6 +36,24 @@ def get_news(category):
 
     return news_results
 
+def search_news(topic):
+    '''
+    Function to search for news by topic
+    '''
+    
+    search_news_url = "https://newsapi.org/v2/everything?q={}&apiKey={}".format(topic, api_key)
+    
+    with urllib.request.urlopen(search_news_url) as url:
+        search_news_data = url.read()
+        search_news_response = json.loads(search_news_data)
+        
+        search_news_results = None
+        if search_news_response["articles"]:
+            search_news_list = search_news_response["articles"]
+            search_news_results = process_results(search_news_list)
+    
+    return search_news_results
+
 
 def process_results(news_list):
     '''
