18dfcae00d445598025baa7143536a0c1821797e
Raymond Yegon <raymondyegon17@gmail.com>
2019-04-12 16:19:32 +0300

    route to search functionality added

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 from flask import render_template, request, redirect, url_for
 from . import main
-from ..request import get_news
+from ..request import get_news, search_news
 
 
 @main.route("/")
@@ -11,5 +11,23 @@ def index():
     top_headlines = get_news("top-headlines")
 
     title = "News headlines"
+    
+    search_news = request.args.get("news_query")
+    
+    if search_news:
+        return redirect(url_for(".search", news_name = search_news))
 
     return render_template("index.html", title = title, top=top_headlines)
+
+
+@main.route("/search/<news_name>")
+def search(news_name):
+    '''
+    View function to display the search results
+    '''
+    news_name_list = news_name.split(" ")
+    news_name_format = "+".join(news_name_list)
+    searched_news = search_news(news_name_format)
+    title = f" Search results for {news_name}"
+    
+    return render_template("search.html", news = searched_news)
