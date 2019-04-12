d7a1b096e87191b0e7e7a94cb6c225b6fdbd35a2
Raymond Yegon <raymondyegon17@gmail.com>
2019-04-12 12:04:53 +0300

    View for top headlines added

---
+++ app/main/views.py
@@ -0,0 +1,15 @@
+from flask import render_template, request, redirect, url_for
+from . import main
+from ..request import get_news
+
+
+@main.route("/")
+def index():
+    '''
+    View root page function that returns the index page and its data
+    '''
+    top_headlines = get_news("top-headlines")
+
+    title = "News headlines"
+
+    return render_template("index.html", title = title, top = top_headlines)
