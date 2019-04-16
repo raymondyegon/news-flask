d6aeec0d2a1bf6e35abc5dd2c95bef97100aa1ba
Raymond Yegon <raymondyegon17@gmail.com>
2019-04-16 14:04:52 +0300

    Sources url added

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 from flask import render_template, request, redirect, url_for
 from . import main
-from ..request import get_news, search_news
+from ..request import get_news, search_news, sources_news
 
 
 @main.route("/")
@@ -13,9 +13,12 @@ def index():
     title = "News headlines"
     
     search_news = request.args.get("news_query")
-    
+    search_sources = request.args.get("news_sources")
     if search_news:
         return redirect(url_for(".search", news_name = search_news))
+    
+    if search_sources:
+        return redirect(url_for(".sources",sources_name= search_sources ) )
 
     return render_template("index.html", title = title, top=top_headlines)
 
@@ -31,3 +34,16 @@ def search(news_name):
     title = f" Search results for {news_name}"
     
     return render_template("search.html", news = searched_news)
+
+@main.route("/sources")
+def sources():
+    '''
+    View function to display sources of news
+    '''
+    source = sources_news()
+    title = f"{sources} news "
+    
+    return render_template("sources.html", source = source)
+
+    
+    
