a11027aedace0801497a73e0ba80b2d699b0dc50
Raymond Yegon <raymondyegon17@gmail.com>
2019-04-16 14:05:43 +0300

    Sources class added

--- app/models.py
+++ app/models.py
@@ -10,4 +10,14 @@ class News:
         self.url = url
         self.urlToImage = urlToImage
         self.content = content
-        
+
+class Sources:
+    '''
+    Sources class to define Sources objects
+    '''
+    def __init__(self, id, name, description, url, category):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.url = url
+        self.category = category
